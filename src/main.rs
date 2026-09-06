dbac86f8de8f4821bab90ba18f592c5ffc03e4e6
frame <marcoanguloe69@gmail.com>
2026-09-05 22:29:13 -0600

    inicialización db client

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,18 @@ mod db;
 mod models;
 mod routes;
 
-use axum::{Router, routing::get};
-
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
+
     let db_client = db::connect_db().await;
-    let app = Router::new().route("/", get(saludo));
+    let app = routes::create_router(db_client);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
-        .expect("Failed to open port:3000");
+        .expect("could not bind to port 3000");
 
     println!("Server running at http://localhost:3000");
 
     axum::serve(listener, app).await.expect("server crashed");
-}
-
-async fn saludo() -> &'static str {
-    "Hola, mundo!"
 }
